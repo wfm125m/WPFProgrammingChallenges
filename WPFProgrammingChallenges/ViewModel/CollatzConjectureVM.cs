ebdad8cc5cf99650f11510417f0dd2571af5334a
wfm125m <wfm125m@gmail.com>
2015-05-07 08:46:36 +0100

    Aded Collatz Conjecture + trying to switch strings into RESX file - now only RESEX added.

---
+++ WPFProgrammingChallenges/ViewModel/CollatzConjectureVM.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFProgrammingChallenges.Helpers;
+
+namespace WPFProgrammingChallenges.ViewModel
+{
+    class CollatzConjectureVM : PropertyChangedHandler
+    {
+
+        private decimal startNumber;
+
+        public decimal StartNumber
+        {
+            get { return startNumber; }
+            set
+            {
+                startNumber = value;
+                Coll();
+            }
+        }
+
+
+        private string resultString;
+
+        public string ResultString
+        {
+            get { return resultString; }
+            set { resultString = value; }
+        }
+
+
+
+        static string AddNumberString(string toAdd, decimal number)
+        {
+            toAdd += number.ToString();
+            toAdd += ", ";
+            return toAdd;
+        }
+
+        private void Coll()
+        {
+            decimal c0;
+            string result = string.Empty;
+
+
+            c0 = startNumber;
+            result = AddNumberString(result, c0);
+            while (c0 != 1)
+            {
+                if (c0 % 2 == 0)
+                {
+                    c0 = c0 / 2;
+                }
+                else
+                {
+                    c0 = 3 * c0 + 1;
+                }
+
+                result = AddNumberString(result, c0);
+            }
+
+            resultString = result;
+            RaisePropertyChangedEvent("ResultString");
+        }
+    }
+}
